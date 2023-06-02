474e3dc4a5fd3449e40576f8d97f53ef03c303ec
thomaskamminga <78427614+thomaskamminga@users.noreply.github.com>
2023-06-02 11:20:45 +0200

    added baselines to the main folder

--- data_parser.py
+++
@@ -1,8 +0,0 @@
-import os
-import json
-
-data_folder = os.path.abspath("KQAPro.IID")
-
-with open(os.path.join(data_folder, "train.json")) as train_file:
-    train_data = json.load(train_file)
-    print(train_data)
